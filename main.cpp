9790e9e1fb6d0f67a99e10a063f537148c400d83
Nick Sweet <nwsweet@umich.edu>
2018-04-09 22:14:17 -0400

    Update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 // A test file for HW 13
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world\n";
+    return 0;
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << i << std::endl;
+    }
+}
